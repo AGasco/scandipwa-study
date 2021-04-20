ae60e511a81ddb66d3689231d2de688534d2e8f3
Antonio Gasco <antoniofgasco@gmail.com>
2021-04-20 19:01:10 +0100

    darkModeProvider-functional

--- packages/scandi-dark-theme/src/plugin/Header.component.plugin.js
+++ packages/scandi-dark-theme/src/plugin/Header.component.plugin.js
@@ -1,9 +1,12 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable simple-import-sort/sort */
 /* eslint-disable react/jsx-curly-spacing */
 /* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 
 import ModeToggleButton from '../component/ModeToggleButton';
+
+import DarkModeProvider from '../component/DarkModeProvider';
 
 import './Header.style.plugin';
 
@@ -16,10 +19,19 @@ export const renderTopMenu = (args, callback, instance) => (
   </>
 );
 
+export const renderRouter = (args, callback, instance) => (
+  <DarkModeProvider>{callback(...args)}</DarkModeProvider>
+);
+
 export default {
   'Component/Header/Component': {
     'member-function': {
       renderTopMenu
+    }
+  },
+  'Component/App/Component': {
+    'member-function': {
+      renderRouter
     }
   }
 };
